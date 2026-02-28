1bfaf4807658cdcadbfbbcec8d09ddd54b042de4
Adam Atienza <adamatnza01@gmail.com>
2026-02-28 15:40:00 -0500

    chore: configure base path for atienza-portfolio repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import solidJs from "@astrojs/solid-js"
 // https://astro.build/config
 export default defineConfig({
   site: "https://ItsAdam01.github.io",
+  base: "/atienza-portfolio",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
